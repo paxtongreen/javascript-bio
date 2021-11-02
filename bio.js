057fdd49ac5c1b222522fc3e65d1468d95f0660e
paxtongreen <88725740+paxtongreen@users.noreply.github.com>
2021-11-02 10:04:42 -0500

    Add files via upload

---
+++ bio.js
@@ -0,0 +1,27 @@
+let myName = document.getElementById('myName');
+console.log(myName);
+
+myName.innerText = 'Paxton S. Green, II';
+
+let bioHeader = document.getElementById('bioHeader');
+
+bioHeader.innerText = 'About Me';
+
+let bioText1 = document.getElementById('bioText1');
+
+bioText1.innerText = 'I\'m a Junior Developer from Pearl, Mississippi. What I love about coding is that there are limitless possibilites as to what you can create.';
+
+let bioText2 = document.getElementById('bioText2');
+
+bioText2.innerText = 'Introvert with extrovert tendencies, knack for solving problems, great at visualization. Food enthusiast, homebody, gamer. Creative who likes to dabble in graphic design.';
+
+let bioText3 = document.getElementById('bioText3');
+
+bioText3.innerText = 'Interested in providing excellent work with a positive attitude.';
+
+let hobby1 = document.getElementById('hobby1');
+
+hobby1.innerText = 'Reading';
+hobby2.innerText = 'Running';
+hobby3.innerText = 'Eating';
+hobby4.innerText = 'Sleeping';
